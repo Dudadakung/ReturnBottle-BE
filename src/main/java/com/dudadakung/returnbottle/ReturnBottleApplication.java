55e467480d35e38fde10144dac64e8ab664fbb92
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2023-12-01 17:06:16 +0900

    first commit

---
+++ src/main/java/com/dudadakung/returnbottle/ReturnBottleApplication.java
@@ -0,0 +1,13 @@
+package com.dudadakung.returnbottle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReturnBottleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReturnBottleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dudadakung/returnbottle/ReturnBottleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dudadakung.returnbottle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReturnBottleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
