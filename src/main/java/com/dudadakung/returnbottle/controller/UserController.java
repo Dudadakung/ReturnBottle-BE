94839546c082fe8b05d124e1e8c4c2f288b35071
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2023-12-05 23:37:32 +0900

    Feat: 로그인 기능 추가

--- src/main/java/com/dudadakung/returnbottle/controller/UserController.java
+++ src/main/java/com/dudadakung/returnbottle/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.dudadakung.returnbottle.controller;
 
+import com.dudadakung.returnbottle.dto.User.request.UserLoginRequestDto;
 import com.dudadakung.returnbottle.dto.User.request.UserSignUpRequestDto;
 import com.dudadakung.returnbottle.global.BaseApiResponse;
 import com.dudadakung.returnbottle.global.SuccessCode;
 import com.dudadakung.returnbottle.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +23,13 @@ public class UserController {
     @PostMapping("/signup")
     public ResponseEntity<BaseApiResponse<?>> signUp(@RequestBody UserSignUpRequestDto userSignUpRequestDto) {
         userService.saveUser(userSignUpRequestDto);
-        return ResponseEntity.status(200).body(BaseApiResponse.of(SuccessCode.CREATED));
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(BaseApiResponse.of(SuccessCode.CREATED));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<BaseApiResponse<?>> login(@RequestBody UserLoginRequestDto userLoginRequestDto) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(BaseApiResponse.of(SuccessCode.OK, userService.loginUser(userLoginRequestDto)));
     }
 }
