965f4323c02ebdcddf110da63d48ea47a65c9f9d
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2023-12-06 19:47:18 +0900

    Feat: 유저의 아이템 목록 불러오기 추가

--- src/main/java/com/dudadakung/returnbottle/repository/UserItemRepository.java
+++ src/main/java/com/dudadakung/returnbottle/repository/UserItemRepository.java
@@ -4,6 +4,11 @@ import com.dudadakung.returnbottle.domain.UserItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserItemRepository extends JpaRepository<UserItem, Long> {
+
+    Optional<List<UserItem>> findAllByUserId(Long id);
 }
